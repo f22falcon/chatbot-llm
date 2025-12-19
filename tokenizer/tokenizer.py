191a3ee2c28d851b96889d38146415807ebbc75e
Tanmoy Giri <tanmoygiri030@gmail.com>
2025-12-19 09:16:04 +0530

    tokenzer relative path update

--- tokenizer/tokenizer.py
+++ tokenizer/tokenizer.py
@@ -1,9 +1,18 @@
 import json
+from pathlib import Path
 
-def tokenizer( file_name="clean_data.txt",path="/media/tanmoy/B406FC5306FC1856/ProjectChatbot/data"):
-    with open (path+"/processed/"+file_name,'r',encoding="utf-8")as f:
+ROOT = Path(__file__).resolve().parents[1]
+DATA = ROOT / "data/processed"
+
+file_path = DATA / "clean_data.txt"
+Vocab_path = DATA /"vocab.json"
+
+
+def tokenizer( Vocab=Vocab_path,path=file_path):
+    
+    with open (path,'r',encoding="utf-8")as f:
         text=f.read()
-    with open(path+"/processed/vocab.json",'r',encoding="utf-8")as f:
+    with open(Vocab,'r',encoding="utf-8")as f:
       vocab = json.load(f)
       word_to_Id = vocab["word_to_id"]
       Token_ids=[ word_to_Id[w] for w in text.lower().split()]
